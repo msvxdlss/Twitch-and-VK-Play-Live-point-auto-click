d880d4710c6eb2c3e666bf4d2769ad68a6a696b3
Димка <ya.lpy@ya.ru>
2026-09-14 11:42:49 +0300

    Delete kick.js

--- kick.js
+++
@@ -1,9 +0,0 @@
-/*
-const CHECK_INTERVAL_MS = 30000;
-const claimBonusInterval = setInterval(() => {
-    const icon = document.querySelector('.POINTNAME');
-    if (icon) {
-        icon.click();
-    }
-}, CHECK_INTERVAL_MS);
-*/
